41f34f0c7ffd4056f35100c959d8675b1fd94470
arpit9839 <94176124+arpit9839@users.noreply.github.com>
2023-04-05 21:49:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [apidata, setApiData] = useState([]);
+  const [currentIdIndex, setCurrentIdIndex] = useState(0);
+
+  const fetchApi = async (id) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    if (!res.ok) {
+      throw new Error("data not found");
+    } else {
+      return res.json();
+    }
+  };
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+
+      const id = currentIdIndex +1 ;
+      
+      console.log(currentIdIndex)
+      fetchApi(id)
+        .then((data) => {
+          setApiData([ data]);
+          setCurrentIdIndex((id));
+          console.log("arpit"+currentIdIndex )
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }, 1000);
+    return () => clearTimeout(interval);
+  }, [currentIdIndex]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {apidata.length > 0 ? (
+        <ul>
+          {apidata.map((data) => (
+            <li key={data.id}>
+              <p><b>name:</b>{data.name}</p>
+              <p><b>email:</b>{data.email}</p>
+              <p><b>address:</b>{apidata.address.map((ad)=>{
+                return(
+                       <>
+                          <h1>
+                          street:{ad.street}
+                          </h1>
+                        
+                        </>     
+                      )
+                         
+              
+            
+                               }) }</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
